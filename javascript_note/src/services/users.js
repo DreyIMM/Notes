bee23b2adddb671c9f00feec38d29a4c65bd308c
DreyIMM <andreydario26@gmail.com>
2022-03-27 17:30:11 -0300

    Componentes na tela de login

--- javascript_note/src/services/users.js
+++ javascript_note/src/services/users.js
@@ -1,7 +1,6 @@
 //Onde é concetrado todas as chamados onde o EndPoint tem o user
 
 import Api from './api';
-import APi from './api';
 
 const UserService ={
     register: (params) => Api.post('/users/register', params)
