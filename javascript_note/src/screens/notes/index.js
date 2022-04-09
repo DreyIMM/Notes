4a6c9d10f782bfa8b54e5a93c6225d692c658787
DreyIMM <andreydario26@gmail.com>
2022-04-09 00:53:33 -0300

    Incluindo o sidebar

--- javascript_note/src/screens/notes/index.js
+++ javascript_note/src/screens/notes/index.js
@@ -1,11 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState} from "react";
 import HeaderLogged from "../../components/header_logged";
-const NotesScreen = () =>( 
+import Notes from "../../components/notes";
+
+const NotesScreen = () =>{  
+
+    //criando um estado compartilhado
+    //quando o método for chamado dentro de HeaderLoged será alterado o valor, e os demais componenets acionados
+    const [isOpen, setIsOpen] = useState(false);
+    
+    return( 
     <Fragment>
-        <HeaderLogged/>
-        Hellow World
+        <HeaderLogged setIsOpen={setIsOpen}/>
+        <Notes setIsOpen={setIsOpen} isOpen={isOpen} />
     </Fragment>
-)
+    
+    
+    
+    );
+}
 
 
 export default NotesScreen;
