cab80e118c12310e79fe7b3c844cfee65dab28e8
DreyIMM <andreydario26@gmail.com>
2022-04-09 00:18:31 -0300

    Header logado no mobile

--- javascript_note/src/screens/notes/index.js
+++ javascript_note/src/screens/notes/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import HeaderLogged from "../../../components/header_logged";
+import HeaderLogged from "../../components/header_logged";
 const NotesScreen = () =>( 
     <Fragment>
         <HeaderLogged/>
