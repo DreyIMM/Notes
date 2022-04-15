67937767727c1b6bcbbacdd1542d9c7d7d60d5e3
DreyIMM <andreydario26@gmail.com>
2022-04-15 13:34:22 -0300

    Excluindo notas

--- javascript_note/src/components/notes/index.js
+++ javascript_note/src/components/notes/index.js
@@ -11,21 +11,29 @@ function Notes(props) {
     const [notes, setNotes] = useState([]);
     const [current_note, setCurrentNote] = useState({ title: "", body: "", id: ""}); 
 
-  //Metodo de listagem de nota
-    async function fetchNotes(){
-      const response = await NotesService.index();
-      if(response.data.length >=1){
-          setNotes(response.data.reverse());
-          setCurrentNote(response.data[0]);
+    //Metodo de listagem de nota
+      async function fetchNotes(){
+        const response = await NotesService.index();
+        if(response.data.length >=1){
+            setNotes(response.data.reverse());
+            setCurrentNote(response.data[0]);
+        }else{
+          setNotes([]);
+        }
       }
+
+    //Metodo para criar uma nova nota e lista
+    const createNote = async () =>{
+        await NotesService.create()
+        fetchNotes();
+    }
+    
+    //Metodo para deletar nova nota
+    const deleteNote = async (note) =>{
+      await NotesService.delete(note._id);
+      fetchNotes()
     }
 
-  //Metodo para criar uma nova nota e lista
-
-  const createNote = async () =>{
-      await NotesService.create()
-      fetchNotes();
-  }
 
     const selectNote = (id) => {
       const note =  notes.find((note) => {
@@ -61,6 +69,7 @@ function Notes(props) {
                 selectNote={selectNote}
                 current_note={current_note} 
                 createNote={createNote}
+                deleteNote={deleteNote}
               />
         </Menu>
 
