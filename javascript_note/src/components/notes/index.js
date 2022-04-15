42b0140f2dccbf13f1cc35151bea0fdbd5aa7e2d
DreyIMM <andreydario26@gmail.com>
2022-04-15 01:16:10 -0300

    Listando as notas

--- javascript_note/src/components/notes/index.js
+++ javascript_note/src/components/notes/index.js
@@ -20,7 +20,7 @@ const Notes = (props) =>{
       }
     }
 
-    const selectNote =  (id) => {
+    const selectNote = (id) => {
       const note =  notes.find((note) => {
         return note._id == id;
       })
@@ -29,7 +29,7 @@ const Notes = (props) =>{
 
     useEffect(() =>{
       fetchNotes();
-    })
+    }, [])
 
 
     return (
