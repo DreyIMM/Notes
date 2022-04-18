92db6e41cda976192ba326d16037736fbd89dbf7
DreyIMM <andreydario26@gmail.com>
2022-04-17 21:06:48 -0300

    Inciando construção da edicao do user

--- javascript_note/src/screens/users/edit/index.js
+++ javascript_note/src/screens/users/edit/index.js
@@ -1,8 +1,40 @@
 import React, { Fragment } from "react";
+import HeaderLoged from '../../../components/header_logged';
+import { Column,Section, Title, Container, Card } from "rbx";
+import logoImage from '../../../assets/images/logo.png';
+import '../../../styles/auth.scss';
+
 
 const UserEditcreen = () =>( 
     <Fragment>
-        Hellow World
+        <HeaderLoged />
+        <Section size="medium" className="edit">
+        <Container>
+          <Column.Group centered>
+            <Column size={6}>
+              <Card>
+                <Card.Content>
+                  <Section>
+                    <Column.Group centered>
+                      <Column size={12}>
+                        <img src={logoImage} />
+                      </Column>
+                    </Column.Group>
+                    <Column.Group>
+                      <Column size={12}>
+                        <Title size={6} className="has-text-grey has-text-centered">
+                          Your notes on the cloud
+                        </Title>
+                      </Column>
+                    </Column.Group>
+                        Form
+                  </Section>
+                </Card.Content>
+              </Card>
+            </Column>
+          </Column.Group>
+        </Container>
+      </Section>
     </Fragment>
 ) 
 
