6ea2a17fea2c5acfb213ba9ea27171c9c1930bbc
DreyIMM <andreydario26@gmail.com>
2022-04-24 21:21:46 -0300

    The first Deploy

--- javascript_note_api/config/database.js
+++ javascript_note_api/config/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+
 //conectando ao banco de dados local
-mongoose.connect('mongodb://localhost/javascriptNote',{
+mongoose.connect(MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true 
 
 }).then(() => console.log('Conection succesful'))
 .catch((err) =>console.log(err));
+
+
