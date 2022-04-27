4fdcacc0c7d03307ad9ed632d7a7efe6ee4eec45
DreyIMM <andreydario26@gmail.com>
2022-04-26 21:35:32 -0300

    The First Deploy in Heroku

--- javascript_note/src/services/api.js
+++ javascript_note/src/services/api.js
@@ -1,6 +1,6 @@
 //Arquivo onde iremos configurar o  AXIOS para chamada de API
 //API roda na porta 3001
 import axios from 'axios'
-const Api = axios.create({baseURL: 'http://localhost:3001'});
+const Api = axios.create({baseURL: process.env.REACT_APP_BASE_API});
 export default Api;
 
